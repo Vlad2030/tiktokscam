900d77a91e16e74f2eae047557482a8aa14660e5
Vlad2030 <61238982+Vlad2030@users.noreply.github.com>
2020-11-08 23:16:23 +0300

    Add files via upload

---
+++ keyboards.py
@@ -0,0 +1,15 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+
+backMenu_btn = KeyboardButton('◀️ В главное меню')
+reviewBtn = InlineKeyboardMarkup(resize_keyboard=True).add(InlineKeyboardButton('🚀 Отзывы!', url='t.me/tiktokgarant'))
+
+# Меню выбора оплаты
+qiwi_btn = KeyboardButton('🥝 QIWI')
+btc_btn = KeyboardButton('📀 BITCOIN (НЕ ДОСТУПНО)')
+card_btn = KeyboardButton('💳 Банковская карта (ВРЕМЕННО НЕ ДОСТУПНО)')
+paymentsMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(qiwi_btn).add(btc_btn).add(card_btn).add(backMenu_btn)
+
+# Меню проверки оплаты
+checkPay_btn = KeyboardButton('💎 Я оплатил')
+cancelPay_btn = KeyboardButton('❌ Отмена')
+checkPayMenu = ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True).add(checkPay_btn).add(cancelPay_btn)
